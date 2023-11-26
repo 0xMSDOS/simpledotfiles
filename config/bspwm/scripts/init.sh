7ec6db06a32753ba8d74fd8a412463561dcc830e
supercode <iammsdos@protonmail.com>
2023-11-26 12:06:52 -0500

    Update sc+

--- config/bspwm/scripts/init.sh
+++ config/bspwm/scripts/init.sh
@@ -4,11 +4,11 @@ CITY="Chiclayo, PE"
 dir="$HOME/.config/bspwm/scripts/weatherV2.py"
 
 case $1 in
-  temp)
-    /bin/python3 $dir --temp "$CITY";;
-  icon)
-    /bin/python3 $dir --icon "$CITY";;
-  city)
-    echo $CITY;;
+    temp)
+        /bin/python3 $dir --temp "$CITY";;
+    icon)
+        /bin/python3 $dir --icon "$CITY";;
+    city)
+        echo $CITY;;
 esac
 
